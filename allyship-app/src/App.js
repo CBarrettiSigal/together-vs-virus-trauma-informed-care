43a56c3fd27c08b1943cde6df8fc8be876d3e71b
Christina BarrettiSigal <CBarrettiSigal@gmail.com>
2020-05-28 15:43:29 -0400

    tinkering with styling and getting frustrated with props

--- allyship-app/src/App.js
+++ allyship-app/src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import OptionButton from './components/OptionButton/OptionButton';
+import classes from './components/OptionButton/OptionButton.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+class App extends Component {
+ state = {
+    buttons: [
+      { id: 1, text: 'Test1' },
+      { id: 2, text: 'Test2' },
+      { id: 3, text: 'Test3' }
+    ]
+  }
+
+render () {
+    const buttons = this.state.buttons.map(button => {
+      return <OptionButton
+        className={classes.OptionButtonContainer}
+        key={button.id}
+        text={button.title} />;
+    });
+
+    return (
+      <div className="App-container">
+        <div className="App-header">
+          <h1>
+            What is Trauma Informed Care?
+          </h1>
+            <img src="allyship_logo.png" className="App-logo" alt="logo" />
+        </div>
+        <div  >
+          {buttons}
+        </div>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href=""
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          rel="noopener noreferrer">Dummy Link
         </a>
-      </header>
-    </div>
-  );
-}
+      </div>
+    );
+  }
+};
 
 export default App;
---
+++ allyship-app/src/components/OptionButton/OptionButton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './OptionButton.module.css';
+
+const OptionButton = (props) => {
+  return (
+    <div className={classes.OptionButton}>test</div>
+  )
+};
+
+
+export default OptionButton;
